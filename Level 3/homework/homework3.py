c673b4c3657fa7c134924ede96902ee7eeafa041
AndriaGvaramadze <gvaramadzeandria@gmail.com>
2024-06-01 02:34:59 +0400

    third commit

---
+++ Level 3/homework/homework3.py
@@ -0,0 +1,24 @@
+name="Andria"
+lastname="Gvaramadze,"
+hobby="Football,Programing"
+
+print (name,lastname,hobby)
+print (10+10)
+
+print (20*20)
+
+print (20*3+9)
+
+print (45+45)
+
+print (4000//200)
+
+print (400*400)
+
+
+print (67*67)
+x=40
+print (x+60)
+print (65**2)
+
+print (1+6*24)
